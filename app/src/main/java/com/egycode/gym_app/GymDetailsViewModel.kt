825a55d3154b761fec2c0fc34838a6a767ef3908
Tarek <tarek.anwar.1120@gmail.com>
2024-02-25 00:46:20 +0200

    remove fun get gym form firebase

--- app/src/main/java/com/egycode/gym_app/GymDetailsViewModel.kt
+++ app/src/main/java/com/egycode/gym_app/GymDetailsViewModel.kt
@@ -16,18 +16,10 @@ class GymDetailsViewModel(
     private val saveStateHandle: SavedStateHandle
 ) : ViewModel() {
 
-    private var gymApiService : GymApiService
     var state by mutableStateOf<Gym?>(null)
     private val gymsDao = GymsDataBase.getDaoInstance(GymsApplication.getApplicationContext())
 
     init {
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://gyms-fe7ee-default-rtdb.firebaseio.com/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-
-        gymApiService = retrofit.create(GymApiService::class.java)
-
         val gymId = saveStateHandle.get<Int>("gym_id") ?: 0
         getGym(gymId)
     }
@@ -41,11 +33,6 @@ class GymDetailsViewModel(
         }catch (e : Exception){
             throw Exception(e.message)
         }
-    }
-
-
-    private suspend fun getGymFromRemoteDB (id: Int) = withContext(Dispatchers.IO){
-        gymApiService.getGymDetails(id).values.first()
     }
 
     private suspend fun getGymFromRoom(id : Int) = withContext(Dispatchers.IO){
